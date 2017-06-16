33dd47aaeb4ec8e663304032fed2682eb7683714
William Ray <wor15@ic.ac.uk>
2017-06-16 13:55:30 +0100

    Tidied up json output

--- constituency/views.py
+++ constituency/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 import json
 
-from django.core import serializers
+# from django.core import serializers
+
+from django.core.serializers.json import Serializer as Builtin_Serializer
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ObjectDoesNotExist
@@ -12,8 +14,15 @@ from .models import Vote
 from .api_utils import verify_pin_and_make_ineligible
 from .api_key_verification import verify, has_vote_permissions
 
+
+class VoteSerializer(Builtin_Serializer):
+    def get_dump_object(self, obj):
+        return self._current
+
+
 def index(request):
     return HttpResponse("The Constituency API is online.")
+
 
 @csrf_exempt
 @verify(lambda: has_vote_permissions)
@@ -21,7 +30,7 @@ def vote_encrypted(request):
     if request.method == 'POST':
         vote_data = json.loads(request.body)
 
-        if all (k in vote_data for k in ('encrpyted_vote', 'pin_code', 'station_id')):
+        if all(k in vote_data for k in ('encrpyted_vote', 'pin_code', 'station_id')):
             encrpyted_vote = vote_data['encrpyted_vote']
             pin_code = vote_data['pin_code']
             station_id = vote_data['station_id']
@@ -30,21 +39,20 @@ def vote_encrypted(request):
                 if encrpyted_vote:
                     Vote.objects.create(vote=encrpyted_vote)
                     return JsonResponse({'success': True,
-                                         'error' : None})
+                                         'error': None})
 
             return JsonResponse({'success': False,
-                                 'error' : 'Voter is ineligible'})
-
+                                 'error': 'Voter is ineligible'})
 
         return JsonResponse({'success': False,
-                         'error' : 'Missing input data'})
+                             'error': 'Missing input data'})
 
 
 def get_votes(request):
     try:
         votes = Vote.objects.all()
-        votes_json = json.loads(serializers.serialize(
-            "json", votes))
+        serializer = VoteSerializer()
+        votes_json = json.loads(serializer.serialize(votes))
 
         return JsonResponse({'success': votes.count() > 0,
                              'votes': votes_json})
